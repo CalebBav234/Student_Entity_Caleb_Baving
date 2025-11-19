658339c0a702199c06a9f7ba1d609d36b65fc863
CalebBav234 <calebmbaving@gmail.com>
2025-11-19 13:06:40 -0400

    Feat: created User Repository and its interface

--- Students/obj/Debug/net9.0/Students.AssemblyInfo.cs
+++ Students/obj/Debug/net9.0/Students.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Students")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76498dd87a4a1a65395322ca265253aaa6ee7693")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f6d86683941685ba67fdc62b50321258b80cdd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Students")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Students")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
