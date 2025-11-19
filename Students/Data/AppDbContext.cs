0f6d86683941685ba67fdc62b50321258b80cdd6
CalebBav234 <calebmbaving@gmail.com>
2025-11-19 13:03:36 -0400

    Feat: created app db context

---
+++ Students/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Students.Models;
+
+namespace Students.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Student> Students => Set<Student>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>();
+            modelBuilder.Entity<Student>();
+        }
+    }
+}
--- Students/obj/Debug/net9.0/Students.AssemblyInfo.cs
+++ Students/obj/Debug/net9.0/Students.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Students")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cef3f09dd137476bdbe22c1cfaba5a9d0ff2e6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76498dd87a4a1a65395322ca265253aaa6ee7693")]
 [assembly: System.Reflection.AssemblyProductAttribute("Students")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Students")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
